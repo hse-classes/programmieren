641b173f39b571f95efc0a8383d3518d1dd932f1
klemensmorbe <klemens@morbe.online>
2026-04-17 07:16:35 +0200

    - add homework(not finished while loo);

--- src/hse/programmieren/block4/aufgaben/Task.java
+++ src/hse/programmieren/block4/aufgaben/Task.java
@@ -1,12 +1,11 @@
 package hse.programmieren.block4.aufgaben;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Task {
 
     static void main() {
-printDistinctCars();
+        forLoop();
     }
 
     // Array
@@ -14,7 +13,8 @@ printDistinctCars();
         // 10 cars
         String[] vehicles = {"BMW", "Audi", "VW", "Mercedes", "Seat", "Skoda", "Ford", "Tesla", "Fiat", "Porsche"};
 
-        for (int i = 0; i < vehicles.length; i+=2) {
+        for (int i = 0; i < vehicles.length; i += 2) {
+
             System.out.println(vehicles[i]);
         }
 
@@ -36,7 +36,7 @@ printDistinctCars();
         vehicles.add("Fiat");
         vehicles.add("Porsche");
 
-        for(int i=vehicles.size()-1;i>=0;i--){
+        for (int i = vehicles.size() - 1; i >= 0; i--) {
             System.out.println(vehicles.get(i));
         }
     }
@@ -55,12 +55,12 @@ printDistinctCars();
         vehicles.add("Tesla");
         vehicles.add("Fiat");
         vehicles.add("Porsche");
-Iterator<String> iterator= vehicles.iterator();
-while (iterator.hasNext()){
-    System.out.println(iterator.next());
-    iterator.remove();
-}
-System.out.println(vehicles);
+        Iterator<String> iterator = vehicles.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+            iterator.remove();
+        }
+        System.out.println(vehicles);
     }
 
     // HashMap
@@ -76,19 +76,19 @@ System.out.println(vehicles);
         vehicles.put("Ford", 33_000);
         vehicles.put("Tesla", 60_000);
         vehicles.put("Fiat", 10_000);
-       vehicles.put("Bobbycar", 20);
+        vehicles.put("Bobbycar", 20);
         vehicles.put("Porsche", 99_000);
-int biggestPrice = 0;
-String nameoftheCar = "undefined";
+        int biggestPrice = 0;
+        String nameoftheCar = "undefined";
 
 
-       for(Map.Entry<String, Integer> entry  : vehicles.entrySet()){
-           if (biggestPrice < entry.getValue()){
-biggestPrice = entry.getValue();
-nameoftheCar = entry.getKey();
-           }
-       }
-System.out.println("Auto: "+ nameoftheCar + "Preis: "+ biggestPrice);
+        for (Map.Entry<String, Integer> entry : vehicles.entrySet()) {
+            if (biggestPrice < entry.getValue()) {
+                biggestPrice = entry.getValue();
+                nameoftheCar = entry.getKey();
+            }
+        }
+        System.out.println("Auto: " + nameoftheCar + "Preis: " + biggestPrice);
 
 
     }
@@ -107,8 +107,47 @@ System.out.println("Auto: "+ nameoftheCar + "Preis: "+ biggestPrice);
         vehicles.add("Tesla");
         vehicles.add("Fiat");
         vehicles.add("Fiat");
-        for( String vehicle: vehicles){
+        for (String vehicle : vehicles) {
             System.out.println(vehicle);
         }
     }
+
+    public static void forLoop() {
+        int currentMileage = 47_500;
+        int serviceInterval = 50_000;
+        int dailyDistance = 150;
+        int day = 1;
+
+        for (; ; day++) {
+            currentMileage += dailyDistance;
+            System.out.println("Tag " + day + ": " + currentMileage);
+            if (currentMileage > serviceInterval) {
+                System.out.println("Service erforderlich");
+                break;
+            }
+        }
+    }
+
+    public static void whileLoop() {
+        System.out.println("Reifendruckkontrolle");
+
+        for (int tire = 1; tire <= 4; tire++) {
+            System.out.println("Prüfe Reifen " + tire);
+
+            int pressure = (new Random().nextInt(10)) + 15;
+            System.out.println("Druck: " + pressure + " bar");
+
+            if (pressure >= 20) {
+                System.out.println("Reifen " + tire + " OK");
+                continue;
+            }
+
+            System.out.println("WARNUNG: Reifen " + tire + " zu wenig Druck!");
+            System.out.println("Reifen " + tire + " muss aufgepumpt werden");
+        }
+
+        System.out.println("Reifendruckkontrolle beendet");
+    }
+
+
 }
