3c9ab8d00f0dba49e9e5f98eb57939c77ce8c929
klemensmorbe <klemens@morbe.online>
2026-04-24 14:53:58 +0200

    - add things we've in the lecture;

--- src/hse/programmieren/block4/MainStud.java
+++ src/hse/programmieren/block4/MainStud.java
@@ -3,15 +3,18 @@ package hse.programmieren.block4;
 public class MainStud {
 
     public static void main(String[] args) {
-        Student student1 = new Student(234, "John", "IT", true);
-        Student student2 = new Student(333, "Sam", "SAGP", false);
 
-        //student1.changeMatkNr(5);
-        System.out.println(student1.name);
-        System.out.println(student2.name);
-        System.out.println(student1);
+        System.out.println(pow(259,3));
+    }
 
-        Student[] myArray = {student1, student2};
-        System.out.println();
+    public static int pow(int a, int b) {
+        if(b == 0) {
+            return 1;
+        }
+        int res = a;
+        for(int i = 1; i < b; i++){
+            res *= a;
+        }
+        return res;
     }
 }
